67db78121b3c1460715480b503744c92a90c1985
R.S. Schermer <roland0507@gmail.com>
2015-02-28 15:59:53 +0100

    Set more s specific angular version, turned listeners into simple functions instead of objects

--- src/rlCommandDispatcher.js
+++ src/rlCommandDispatcher.js
@@ -2,7 +2,7 @@
 
 (function() {
   var rlCommandDispatcherModule = angular.module('rlCommandDispatcher', []);
-  
+
   rlCommandDispatcherModule
     .provider('commandDispatcher', function () {
       var commands = {};
@@ -12,95 +12,95 @@
         beforeExecuteListeners: {},
         afterExecuteListeners: {}
       };
-      
+
       var commandDispatcher = function (commands, listeners) {
         this.dispatch = function (commandName, commandParams) {
           if (!commands[commandName]) {
             throw new Error('Unable to find command with name "'+ commandName +'". Did you register it with the '
               + 'commandDispatcherProvider before registering the listener?');
           }
-          
+
           var command = angular.copy(commands[commandName]);
-          
+
           if (typeof command.initialize === 'function') {
             if (listeners.beforeInitializeListeners[commandName]) {
               angular.forEach(listeners.beforeInitializeListeners[commandName], function (listener) {
-                listener.notify(command, 'beforeInitialize', commandName);
+                listener(command, 'beforeInitialize', commandName);
               });
             }
-            
+
             command.initialize(commandParams);
-            
+
             if (listeners.afterInitializeListeners[commandName]) {
               angular.forEach(listeners.afterInitializeListeners[commandName], function (listener) {
-                listener.notify(command, 'afterInitialize', commandName);
+                listener(command, 'afterInitialize', commandName);
               });
             }
           }
-          
+
           if (listeners.beforeExecuteListeners[commandName]) {
             angular.forEach(listeners.beforeExecuteListeners[commandName], function (listener) {
-              listener.notify(command, 'beforeExecute', commandName);
+              listener(command, 'beforeExecute', commandName);
             });
           }
-          
+
           command.execute();
-          
+
           if (listeners.afterExecuteListeners[commandName]) {
             angular.forEach(listeners.afterExecuteListeners[commandName], function (listener) {
-              listener.notify(command, 'afterExecute', commandName);
+              listener(command, 'afterExecute', commandName);
             });
           }
         };
       };
-      
+
       this.registerCommand = function (commandName, command) {
         if (typeof command.execute !== 'function') {
           throw new Error("Command must have an 'execute' method.");
         }
-        
+
         commands[commandName] = command;
       };
-      
+
       this.registerListener = function (listenerType, commandName, listener) {
-        if (typeof listener.notify !== 'function') {
-          throw new Error("Listener must have a 'notify' method.");
+        if (typeof listener !== 'function') {
+          throw new Error("Listener must be a function.");
         }
-        
+
         switch (listenerType) {
           case 'beforeInitialize':
             if (!listeners.beforeInitializeListeners[commandName]) {
               listeners.beforeInitializeListeners[commandName] = [];
             }
-            
+
             listeners.beforeInitializeListeners[commandName].push(listener);
             break;
           case 'afterInitialize':
             if (!listeners.afterInitializeListeners[commandName]) {
               listeners.afterInitializeListeners[commandName] = [];
             }
-            
+
             listeners.afterInitializeListeners[commandName].push(listener);
             break;
           case 'beforeExecute':
             if (!listeners.beforeExecuteListeners[commandName]) {
               listeners.beforeExecuteListeners[commandName] = [];
             }
-            
+
             listeners.beforeExecuteListeners[commandName].push(listener);
             break;
           case 'afterExecute':
             if (!listeners.afterExecuteListeners[commandName]) {
               listeners.afterExecuteListeners[commandName] = [];
             }
-            
+
             listeners.afterExecuteListeners[commandName].push(listener);
             break;
           default:
-            throw new Error('"'+ listenerType +'" is an unsupported listener type.');
+            throw new Error('"'+ listenerType +'" is not a supported listener type.');
         }
       };
-      
+
       this.$get = function () {
         return new commandDispatcher(commands, listeners);
       };
--- test/rlCommandDispatcherSpec.js
+++ test/rlCommandDispatcherSpec.js
@@ -1,19 +1,19 @@
 describe("rlCommandDispatcher", function() {
   var cdProvider, cdService;
-  
+
   beforeEach(function () {
     angular.module('rlCommandDispatcher.test', ['rlCommandDispatcher'])
       .config(function (commandDispatcherProvider) {
         cdProvider = commandDispatcherProvider;
       });
-    
+
     angular.mock.module('rlCommandDispatcher', 'rlCommandDispatcher.test');
-    
+
     angular.mock.inject(function($injector) {
       cdService = $injector.invoke(cdProvider.$get);
     });
   });
-  
+
   describe('provider', function () {
     it('should throw an error on command without execute method', function () {
       expect(function () {
@@ -22,254 +22,190 @@ describe("rlCommandDispatcher", function() {
         });
       }).toThrow("Command must have an 'execute' method.");
     });
-    
-    it('should throw an error on listener without notify method', function () {
+
+    it('should throw an error on listener that is not a function', function () {
       expect(function () {
-        cdProvider.registerListener('beforeExecute', 'someCommand', {
-          someMethod: function () {}
-        });
-      }).toThrow("Listener must have a 'notify' method.");
+        cdProvider.registerListener('beforeExecute', 'someCommand', 'notAFunction');
+      }).toThrow("Listener must be a function.");
     });
-    
+
     it('should throw an error on unsupported listener type', function () {
       expect(function () {
-        cdProvider.registerListener('unsupportedListenerType', 'someCommand', {
-          notify: function () {}
-        });
-      }).toThrow('"unsupportedListenerType" is an unsupported listener type.');
+        cdProvider.registerListener('unsupportedListenerType', 'someCommand', function () {});
+      }).toThrow('"unsupportedListenerType" is not a supported listener type.');
     });
   });
-  
+
   describe('service', function () {
     it('should call the commands execute method on dispatch', function () {
       var command = {
         name: null,
-  
+
         execute: function () {
           console.log('Hello '+ this.name || 'World' + '!');
         }
       };
-      
+
       spyOn(command, 'execute');
-      
+
       cdProvider.registerCommand('sayHello', command);
       cdService.dispatch('sayHello');
-      
+
       expect(command.execute).toHaveBeenCalled();
     });
-    
+
     it('should call the commands initialize method when defined', function () {
       var command = {
         name: null,
-        
+
         initialize: function () {
           this.name = 'Bob';
         },
-        
-        execute: function () {
-          console.log('Hello '+ this.name || 'World' + '!');
-        }
+
+        execute: function () { }
       };
-      
+
       spyOn(command, 'initialize');
-      
+
       cdProvider.registerCommand('sayHello', command);
       cdService.dispatch('sayHello');
-      
+
       expect(command.initialize).toHaveBeenCalled();
     });
-    
+
     it('should pass a paramater hash to the initialize method', function () {
       var command = {
         name: null,
-        
+
         initialize: function (params) {
           this.name = params.name;
         },
-        
-        execute: function () {
-          console.log('Hello '+ this.name || 'World' + '!');
-        }
+
+        execute: function () { }
       };
-      
+
       spyOn(command, 'initialize');
-      
+
       cdProvider.registerCommand('sayHello', command);
       cdService.dispatch('sayHello', {name: 'Bob'});
-      
+
       expect(command.initialize).toHaveBeenCalledWith({name: 'Bob'});
     });
-    
+
     it('should not call an initialize listener when there is no initiaze method on the command', function () {
       cdProvider.registerCommand('someCommand', {
         execute: function () {}
       });
-      
-      var beforeInitializeListener = {
-        notify: function () {}
-      };
-      
-      var afterInitializeListener = {
-        notify: function () {}
-      };
-      
-      cdProvider.registerListener('beforeInitialize', 'someCommand', beforeInitializeListener);
-      cdProvider.registerListener('beforeInitialize', 'someCommand', afterInitializeListener);
-      
-      spyOn(beforeInitializeListener, 'notify');
-      spyOn(afterInitializeListener, 'notify');
-      
+
+      var beforeInitializeListenerSpy = jasmine.createSpy('beforeInitializeListenerSpy');
+      var afterInitializeListenerSpy = jasmine.createSpy('afterInitializeListenerSpy');
+
+      cdProvider.registerListener('beforeInitialize', 'someCommand', beforeInitializeListenerSpy);
+      cdProvider.registerListener('beforeInitialize', 'someCommand', afterInitializeListenerSpy);
+
       cdService.dispatch('someCommand');
-      
-      expect(beforeInitializeListener.notify).not.toHaveBeenCalled();
-      expect(afterInitializeListener.notify).not.toHaveBeenCalled();
+
+      expect(beforeInitializeListenerSpy).not.toHaveBeenCalled();
+      expect(afterInitializeListenerSpy).not.toHaveBeenCalled();
     });
-    
+
     it('should call all before-initialize listeners when there is an initialize command', function () {
       cdProvider.registerCommand('someCommand', {
         initialize: function () {},
-        
+
         execute: function () {}
       });
-      
-      var beforeInitializeListener = {
-        notify: function () {}
-      };
-      
-      var otherBeforeInitializeListener = {
-        notify: function () {}
-      };
-      
-      cdProvider.registerListener('beforeInitialize', 'someCommand', beforeInitializeListener);
-      cdProvider.registerListener('beforeInitialize', 'someCommand', otherBeforeInitializeListener);
-      
-      spyOn(beforeInitializeListener, 'notify');
-      spyOn(otherBeforeInitializeListener, 'notify');
-      
+
+      var beforeInitializeListenerSpy = jasmine.createSpy('beforeInitializeListenerSpy');
+      var otherBeforeInitializeListenerSpy = jasmine.createSpy('otherBeforeInitializeListenerSpy');
+
+      cdProvider.registerListener('beforeInitialize', 'someCommand', beforeInitializeListenerSpy);
+      cdProvider.registerListener('beforeInitialize', 'someCommand', otherBeforeInitializeListenerSpy);
+
       cdService.dispatch('someCommand');
-      
-      expect(beforeInitializeListener.notify).toHaveBeenCalled();
-      expect(otherBeforeInitializeListener.notify).toHaveBeenCalled();
+
+      expect(beforeInitializeListenerSpy).toHaveBeenCalled();
+      expect(otherBeforeInitializeListenerSpy).toHaveBeenCalled();
     });
-    
+
     it('should call all after-initialize listeners when there is an initialize command', function () {
       cdProvider.registerCommand('someCommand', {
         initialize: function () {},
-        
+
         execute: function () {}
       });
-      
-      var afterInitializeListener = {
-        notify: function () {}
-      };
-      
-      var otherAfterInitializeListener = {
-        notify: function () {}
-      };
-      
-      cdProvider.registerListener('afterInitialize', 'someCommand', afterInitializeListener);
-      cdProvider.registerListener('afterInitialize', 'someCommand', otherAfterInitializeListener);
-      
-      spyOn(afterInitializeListener, 'notify');
-      spyOn(otherAfterInitializeListener, 'notify');
-      
+
+      var afterInitializeListenerSpy = jasmine.createSpy('afterInitializeListenerSpy');
+      var otherAfterInitializeListenerSpy = jasmine.createSpy('otherAfterInitializeListenerSpy');
+
+      cdProvider.registerListener('afterInitialize', 'someCommand', afterInitializeListenerSpy);
+      cdProvider.registerListener('afterInitialize', 'someCommand', otherAfterInitializeListenerSpy);
+
       cdService.dispatch('someCommand');
-      
-      expect(afterInitializeListener.notify).toHaveBeenCalled();
-      expect(otherAfterInitializeListener.notify).toHaveBeenCalled();
+
+      expect(afterInitializeListenerSpy).toHaveBeenCalled();
+      expect(otherAfterInitializeListenerSpy).toHaveBeenCalled();
     });
-    
+
     it('should call all before-execute listeners', function () {
       cdProvider.registerCommand('someCommand', {
         execute: function () {}
       });
-      
-      var beforeExecuteListener = {
-        notify: function () {}
-      };
-      
-      var otherBeforeExecuteListener = {
-        notify: function () {}
-      };
-      
-      cdProvider.registerListener('beforeExecute', 'someCommand', beforeExecuteListener);
-      cdProvider.registerListener('beforeExecute', 'someCommand', otherBeforeExecuteListener);
-      
-      spyOn(beforeExecuteListener, 'notify');
-      spyOn(otherBeforeExecuteListener, 'notify');
-      
+
+      var beforeExecuteListenerSpy = jasmine.createSpy('beforeExecuteListenerSpy');
+      var otherBeforeExecuteListenerSpy = jasmine.createSpy('otherBeforeExecuteListenerSpy');
+
+      cdProvider.registerListener('beforeExecute', 'someCommand', beforeExecuteListenerSpy);
+      cdProvider.registerListener('beforeExecute', 'someCommand', otherBeforeExecuteListenerSpy);
+
       cdService.dispatch('someCommand');
-      
-      expect(beforeExecuteListener.notify).toHaveBeenCalled();
-      expect(otherBeforeExecuteListener.notify).toHaveBeenCalled();
+
+      expect(beforeExecuteListenerSpy).toHaveBeenCalled();
+      expect(otherBeforeExecuteListenerSpy).toHaveBeenCalled();
     });
-    
+
     it('should call all after-execute listeners', function () {
       cdProvider.registerCommand('someCommand', {
         execute: function () {}
       });
-      
-      var afterExecuteListener = {
-        notify: function () {}
-      };
-      
-      var otherAfterExecuteListener = {
-        notify: function () {}
-      };
-      
-      cdProvider.registerListener('afterExecute', 'someCommand', afterExecuteListener);
-      cdProvider.registerListener('afterExecute', 'someCommand', otherAfterExecuteListener);
-      
-      spyOn(afterExecuteListener, 'notify');
-      spyOn(otherAfterExecuteListener, 'notify');
-      
+
+      var afterExecuteListenerSpy = jasmine.createSpy('afterExecuteListenerSpy');
+      var otherAfterExecuteListenerSpy = jasmine.createSpy('otherAfterExecuteListenerSpy');
+
+      cdProvider.registerListener('afterExecute', 'someCommand', afterExecuteListenerSpy);
+      cdProvider.registerListener('afterExecute', 'someCommand', otherAfterExecuteListenerSpy);
+
       cdService.dispatch('someCommand');
-      
-      expect(afterExecuteListener.notify).toHaveBeenCalled();
-      expect(otherAfterExecuteListener.notify).toHaveBeenCalled();
+
+      expect(afterExecuteListenerSpy).toHaveBeenCalled();
+      expect(otherAfterExecuteListenerSpy).toHaveBeenCalled();
     });
-    
+
     it('should pass the command, the listener type and the command name to listeners', function () {
       var command = {
         initialize: function () {},
-        
+
         execute: function () {}
       };
-      
+
       cdProvider.registerCommand('someCommand', command);
-      
-      var beforeInitializeListener = {
-        notify: function () {}
-      };
-      
-      var afterInitializeListener = {
-        notify: function () {}
-      };
-      
-      var beforeExecuteListener = {
-        notify: function () {}
-      };
-      
-      var afterExecuteListener = {
-        notify: function () {}
-      };
-      
-      cdProvider.registerListener('beforeInitialize', 'someCommand', beforeInitializeListener);
-      cdProvider.registerListener('afterInitialize', 'someCommand', afterInitializeListener);
-      cdProvider.registerListener('beforeExecute', 'someCommand', beforeExecuteListener);
-      cdProvider.registerListener('afterExecute', 'someCommand', afterExecuteListener);
-      
-      spyOn(beforeInitializeListener, 'notify');
-      spyOn(afterInitializeListener, 'notify');
-      spyOn(beforeExecuteListener, 'notify');
-      spyOn(afterExecuteListener, 'notify');
-      
+
+      var beforeInitializeListenerSpy = jasmine.createSpy('beforeInitializeListenerSpy');
+      var afterInitializeListenerSpy = jasmine.createSpy('afterInitializeListenerSpy');
+      var beforeExecuteListenerSpy = jasmine.createSpy('beforeExecuteListenerSpy');
+      var afterExecuteListenerSpy = jasmine.createSpy('afterExecuteListenerSpy');
+
+      cdProvider.registerListener('beforeInitialize', 'someCommand', beforeInitializeListenerSpy);
+      cdProvider.registerListener('afterInitialize', 'someCommand', afterInitializeListenerSpy);
+      cdProvider.registerListener('beforeExecute', 'someCommand', beforeExecuteListenerSpy);
+      cdProvider.registerListener('afterExecute', 'someCommand', afterExecuteListenerSpy);
+
       cdService.dispatch('someCommand');
-      
-      expect(beforeInitializeListener.notify).toHaveBeenCalledWith(command, 'beforeInitialize', 'someCommand');
-      expect(afterInitializeListener.notify).toHaveBeenCalledWith(command, 'afterInitialize', 'someCommand');
-      expect(beforeExecuteListener.notify).toHaveBeenCalledWith(command, 'beforeExecute', 'someCommand');
-      expect(afterExecuteListener.notify).toHaveBeenCalledWith(command, 'afterExecute', 'someCommand');
+
+      expect(beforeInitializeListenerSpy).toHaveBeenCalledWith(command, 'beforeInitialize', 'someCommand');
+      expect(afterInitializeListenerSpy).toHaveBeenCalledWith(command, 'afterInitialize', 'someCommand');
+      expect(beforeExecuteListenerSpy).toHaveBeenCalledWith(command, 'beforeExecute', 'someCommand');
+      expect(afterExecuteListenerSpy).toHaveBeenCalledWith(command, 'afterExecute', 'someCommand');
     });
   });
 });
